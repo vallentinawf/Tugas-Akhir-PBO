e40d70d8cf11b81f7524f2e1ab749555e8cbc525
Vallentina Wahyu F <79146280+vallentinawf@users.noreply.github.com>
2021-06-08 21:59:14 +0800

    Add files via upload

---
+++ KopilihTests/MainMenuTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kopilih;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kopilih.Tests
+{
+    [TestClass()]
+    public class MainMenuTests
+    {
+        [TestMethod()]
+        public void ConclusingTest()
+        {
+            
+            MainMenu mainMenu = new MainMenu();
+            var totalEx = 60000;
+            mainMenu.Conclusing(totalEx);
+            Assert.AreEqual(60000, totalEx);
+           
+        }
+
+    }
+}
